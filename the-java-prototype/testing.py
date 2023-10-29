857be9623fcf2124e97248132acb586b629cddf1
Adron Hall <adron@thrashingcode.com>
2023-10-28 20:17:53 -0700

    The running GraphQL API Hello World example.

--- the-java-prototype/testing.py
+++ the-java-prototype/testing.py
@@ -1,35 +1,16 @@
 from flask import Flask
 from flask_graphql import GraphQLView
-from pymongo import MongoClient
 import graphene
-from graphene import ObjectType
 
-app = Flask(__name__)
+class Query(graphene.ObjectType):
+    hello = graphene.String(description='A typical hello world')
 
-# Connect to MongoDB
-client = MongoClient('localhost', 27017)
-db = client['your_database_name']
-
-class Query(ObjectType):
-    pass
-
-# Dynamically create GraphQL fields based on MongoDB collections and their properties
-for collection_name in db.list_collection_names():
-    collection = db[collection_name]
-    document = collection.find_one()
-    if document:
-        fields = {key: graphene.String() for key in document.keys()}
-        # Create a dynamic GraphQL ObjectType for each collection
-        collection_type = type(collection_name.capitalize(), (ObjectType,), fields)
-        setattr(Query, collection_name, graphene.Field(collection_type, id=graphene.String()))
-
-        def resolver(self, info, id):
-            return db[collection_name].find_one({"_id": id})
-
-        setattr(Query, f"resolve_{collection_name}", resolver)
+    def resolve_hello(self, info):
+        return 'Hello World'
 
 schema = graphene.Schema(query=Query)
 
+app = Flask(__name__)
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
 if __name__ == '__main__':
